5b91ae3a8ddbed0cc789e94cbeb1d60641724314
Sny96 <nat15sn2@student.lu.se>
2020-06-01 18:08:17 +0200

    Now the code is functional. parameters is added

--- 1D-CA.py
+++ 1D-CA.py
@@ -1,32 +1,35 @@
 import numpy as np
 import random
+from parameters import *
+import matplotlib.pyplot as plt
 
 class Environment():
    
    def init(self):
-      self.seed = 1
+      self.seed = seed
       random.seed(self.seed)
       self.eps = 1E-3
       
-      self.r_x = 0.4
-      self.r_y = 0.3
-      self.max_move = 5
-      self.array_length = 100
+      self.r_x = r_x
+      self.r_y = r_y
+      self.max_move = max_move
+      self.array_length = array_length
       
       self.prey_list = [0]*self.array_length
       self.pred_list = [0]*self.array_length
-      self.old_pred_list = [0]*self.array_length
       
       self.time=0
-   
-   
-   def update():
-      self.time+=1
-      self.old_pred_list = [x for x in pred_list]
       
-      move_pred() #every pred moves
-      interaction() #pred health->1, prey->0 when interacting 
-      update_rep_death() #increase prey health by r_x, reduce pred health by r_y
+   def init_pred_prey(self, n_prey, n_pred):
+      index_pred = [index for index, value in enumerate(self.pred_list)]
+      index_prey = [index for index, value in enumerate(self.prey_list)]
+      
+      r_pred = random.choices(index_pred, k=n_pred)
+      r_prey = random.choices(index_prey, k=n_prey)
+      for i in r_pred:
+         self.pred_list[i] = 1
+      for i in r_prey:
+         self.prey_list[i] = 1
 
    
    def get_preylist(self):
@@ -35,49 +38,41 @@ class Environment():
    def get_predlist(self):
       return self.pred_list
    
-   def move_all_pred(self): #How should we move our predators?? random?
-      pred_index_list = [index for index, value in enumerate(self.pred_list) if value == 1]
-      index_index_list = [index for index, value in enumerate(pred_index_list)]
-      direction_list=[random.choice([-1,1]) for x in index_list]
-      
-      for i in range(len(index_list)):
-         dist = index_list[i+direction_list[i]]-index_list[i]
+   def get_creatures(self):
+      return sum(self.prey_list), sum(self.pred_list)
    
-   def move_one_pred(self, i, dist):
+   def move_all_pred(self): #chosen at random, moved in random direction
+      pred_index_list = [index for index, value in enumerate(self.pred_list) if value == 1]
       
+      for i in range(len(pred_index_list)):
+         
+         pred_index_list = [index for index, value in enumerate(self.pred_list) if value == 1]
+         r = random.choice(pred_index_list)
+         direction = random.choice([-1,1])
+         dist = self.pred_list[(r+direction)%self.array_length]-self.pred_list[r]
+         move = max(self.max_move, abs(dist)-1)
+         self.pred_list[r]=0
+         self.pred_list[(r+move*direction)%self.array_length] = 1
       
       
    def update_rep_death(self):
-      index_list_prey = []
-      index_list_pred
-      
-      for i in range(self.array_length):
-         if random.random()<self.r_x and self.prey_list[i]==1:
+      index_pred = [index for index, value in enumerate(self.pred_list)]
+      index_prey = [index for index, value in enumerate(self.prey_list)]
+      random.shuffle(index_prey)
+      random.shuffle(index_pred)
             
-         if random.random()<self.r_y and self.pred_list[i]==1:
-            self.pred_list[i] = 0
-            
-      for i in random.shuffle(index_list_prey):
+      for i in index_prey:
          if random.random()<self.r_x:
-            spawn_new_prey(i)
+            self.spawn_new_prey(i)
             
-      for i in random.shuffle(index_list_prey):
+      for i in index_prey:
          if random.random()<self.r_y:
             self.pred_list[i] = 0
-      
-   def interaction(self):
-      index_list = []
-      for i in range(self.array_length):
-         if (self.prey_list[i] != 0 and self.pred_list != 0):
-            index_list.append(i)
-      
-      for index in random.shuffle(index_list):   
-            self.prey_list[index] = 0
-            spawn_new_pred(index)
+            
             
    def spawn_new_prey(self, i): 
       if self.prey_list[i] == 0:
-         self.prey_list = 1
+         self.prey_list[i] = 1
       else:
          r = random.choice([-1,1])
          if self.prey_list[(i+r)%self.array_length]==0:
@@ -86,28 +81,65 @@ class Environment():
             self.prey_list[(i-r)%self.array_length] = 1
    
    def spawn_new_pred(self, i): 
-      if self.pred_list[i] == 0:
-         self.pred_list = 1
-      else:
-         r = random.choice([-1,1])
-         if self.pred_list[(i+r)%self.array_length]==0:
-            self.pred_list[(i+r)%self.array_length] = 1
-         elif self.pred_list[(i-r)%self.array_length]==0:
-            self.pred_list[(i-r)%self.array_length] = 1
-   
-   def change_prey(index,num):
+      r = random.choice([-1,1])
+      if self.pred_list[(i+r)%self.array_length]==0:
+         self.pred_list[(i+r)%self.array_length] = 1
+      elif self.pred_list[(i-r)%self.array_length]==0:
+         self.pred_list[(i-r)%self.array_length] = 1
+            
+   def interaction(self):
+      index_list = []
+      for i in range(self.array_length):
+         if (self.prey_list[i] == 1 and self.pred_list[i] == 1):
+            index_list.append(i)
+      random.shuffle(index_list)
+      for index in index_list:   
+            self.prey_list[index] = 0
+            self.spawn_new_pred(index)
+
+         
+   def update(self):
+      self.time+=1
+      self.move_all_pred() #every pred moves
+      self.interaction() #pred health->1, prey->0 when interacting 
+      self.update_rep_death() #increase prey health by r_x, reduce pred health by r_y
+
+def plotting_system(pop_matrix):
+      fig = plt.figure(num=None, figsize=(6, 13), dpi=80, facecolor='w', edgecolor='k')
+      plt.xlabel('Space')
+      plt.ylabel('Time')
+      plt.imshow(pop_matrix, cmap='Greys', origin='lower')
       
-      index_list = [index for index, value in enumerate(self.prey_list) if value == 0]
-      #TODO: decide initialization rule
-      if num==0:
-         self.prey_list[index] = 0
-      if num==1:
-         self.prey_list[index] = 1
+def plotting_population(num_pred_list, num_prey_list):
+    plt.xlabel('Predators')
+    plt.ylabel('Prey')
+    plt.plot(num_pred_list, num_prey_list, 'ro')
 
-
-if __name__ == "__main__":
+def main():
    a = Environment()
    a.init()
-   b = a.get_preylist()
-   print(b)
+   a.init_pred_prey(prey_num,pred_num)
+   predator_list = [[x for x in a.get_predlist()]]
+   prey_list = [[x for x in a.get_preylist()]]
+   
+   c = a.get_creatures()
+   num_prey = [c[0]]
+   num_pred = [c[1]]
+#   
+   for i in range(time_of_sim):
+      a.update()
+
+      prey_list.append([x for x in a.get_preylist()])
+      predator_list.append([x for x in a.get_predlist()])
+      
+      c = a.get_creatures()
+      num_prey.append(c[0])
+      num_pred.append(c[1])
+      
+   plotting_population(num_pred, num_prey)
+   plotting_system(prey_list)
+
+if __name__ == "__main__":
+   main()
+      
    
---
+++ parameters.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 16:26:19 2020
+
+@author: sny
+"""
+
+time_of_sim = 200
+array_length = 500
+seed = 3
+r_x = 0.05
+r_y = 0.1
+prey_num = 20
+pred_num = 10
+max_move=3
